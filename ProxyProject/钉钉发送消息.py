3f191032799cbc45c56e16cf8d54980a3dc54521
Nell3582 <qianye1203@outlook.com>
2021-02-02 21:15:26 +0800

    test

--- ProxyProject/钉钉发送消息.py
+++ ProxyProject/钉钉发送消息.py
@@ -1,14 +1,34 @@
+
 import appex
 import clipboard
 import console
 import requests
 import json
+import pyperclip
+import time
+import hmac
+import hashlib
+import base64
+import urllib.parse
+
+timestamp = str(round(time.time() * 1000))
+secret = 'SECbf540a5693061ed74b38299dbf8dfaa79e295d29bfbefea2117307884502e129'  #这里填的就是上面获取的加签密钥
+secret_enc = secret.encode('utf-8')
+string_to_sign = '{}\n{}'.format(timestamp, secret)
+string_to_sign_enc = string_to_sign.encode('utf-8')
+hmac_code = hmac.new(secret_enc, string_to_sign_enc,
+                     digestmod=hashlib.sha256).digest()
+sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+# print(timestamp)
+# print(sign)
+
+# text = pyperclip.paste()
 
 
 class dd:
     def req(self):
         #把第二步中获取到的 timestamp和sign拼接到URL中
-        url = ('https://oapi.dingtalk.com/robot/send?access_token=9d35a417fd13301e046c31eb6deb29c47b769531a1e9d5e73b8a37d56dd1f2c1&timestamp=1612263003859&sign=B5Q86Vohcwn5rWKcItthGWGkY4kWudr1CyW0vd7kakk%3D')
+        url = (f'https://oapi.dingtalk.com/robot/send?access_token=9d35a417fd13301e046c31eb6deb29c47b769531a1e9d5e73b8a37d56dd1f2c1&timestamp={timestamp}&sign={sign}')
         h = {
             'content-type':
             'application/json',
@@ -28,7 +48,8 @@ class dd:
         })
         req = requests.post(url, data=d, headers=h)
         data = req.json()
-        if data['error_code'] == 0 and data['errmsg'] == 'ok':
+        print(type(data))
+        if data['errcode'] == 0 and data['errmsg'] == 'ok':
             console.hud_alert('发送成功,请到客户端查看相关信息')
             print('发送成功,请到客户端查看相关信息')
         print(req.text)
@@ -45,7 +66,7 @@ class dd:
 #     dd().req()
 
 text = appex.get_text()
-console.hud_alert('已发送,请查收')
+# console.hud_alert('已发送,请查收')
 
 if __name__ == '__main__':
     if appex.is_running_extension():
